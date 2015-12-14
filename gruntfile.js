0189c2425dfedb4a4e65819b8afd388351412e5d
Mats Grimsgaard <mats@mgv.no>
2015-12-14 11:55:20 +0100

    Renset opp ubrukte filer og lagt til bower

--- gruntfile.js
+++ gruntfile.js
@@ -3,41 +3,36 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    bower_concat: {
+      all: {
+        dest: 'build/_bower.js',
+        cssDest: 'build/_bower.css'
+      }
+    },
     uglify: {
       build: {
         options: {
           banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
         },
         files: {
-          'build/upload.min.js': ['src/chartjs.js', 'src/tc-angular-chartjs.js', 'src/papa.js', 'src/upload.js']
+          'build/upload.min.js': ['build/_bower.js', 'src/upload.js']
         }
       }
     },
-	jade: {
-		compile: {
-			files: {
-				"upload.html": "views/upload.jade"
-			}
-		}
-	},
-  preprocess: {
-    dist: {
-      src: 'upload.html',
-      dest: 'build/index.html',
-      options: {
-        context : {
-          DEBUG: false
-        }
-      }
-    }
-  }
+  	jade: {
+  		compile: {
+  			files: {
+  				"build/index.html": "views/upload.jade"
+  			}
+  		}
+  	}
   });
 
+  grunt.loadNpmTasks('grunt-bower-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-preprocess');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'jade', 'preprocess:dist']);
+  grunt.registerTask('default', ['bower_concat', 'uglify', 'jade']);
 
 };
